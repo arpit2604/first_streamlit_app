83fe7773db46146a78c4135608b2c3bcce62a2e5
Arpit Agarwal <arpit2604@gmail.com>
2022-07-27 13:36:22 +0530

    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit Load List Old'):
   streamlit.dataframe(my_data_rows)
 #streamlit.stop() 
 streamlit.header("The fruit load list contains:")
