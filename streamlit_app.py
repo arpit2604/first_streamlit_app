1a19901ab69c90fc0a882d4eb9efe853a25b58ac
Arpit Agarwal <arpit2604@gmail.com>
2022-07-27 13:30:26 +0530

    stop added

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,13 +58,13 @@ except URLError as e:
 
 
 
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("select * from fruit_load_list")
-#my_data_rows = my_cur.fetchall()
-#streamlit.header("The fruit load list contains:")
-#streamlit.dataframe(my_data_rows)
-
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+streamlit.stop() 
 streamlit.header("The fruit load list contains:")
 #Snowflake-related-functions
 def get_fruit_load_list():
